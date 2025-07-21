b9a9d359adca8fdfcc12d52b238d4d5c017789ff
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-21 15:32:40 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+/**	op-asset-git:/index.php
+ *
+ * @created    2025-07-21
+ * @version    1.0
+ * @package    op-asset-git
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+//	...
+if(!OP::isAdmin() ){
+	OP::Html('Your not admin.');
+	return;
+}
+
+//	...
+$command = OP::Request('command');
+
+//	...
+switch( $command ){
+	case 'update':
+	case 'fetch':
+	case 'pick':
+		//	...
+		if(!file_exists($path = realpath("./{$command}.php")) ){
+			OP::Html("File does not exists: {$path}");
+		}
+
+		//	...
+		chdir(_ROOT_GIT_);
+
+		//	...
+		if( include($path) ){
+			return;
+		}
+	break;
+}
+
+?>
+
+# Please select follow command:
+
+ * update
+ * fetch
+ * pick
