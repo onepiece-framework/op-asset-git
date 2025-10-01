59e6cec92dcf1c59a89ef3c0f85e75c2f76714ef
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-01 15:44:04 +0900

    Add: pull.php | PHPDoc

---
+++ pull.php
@@ -0,0 +1,25 @@
+<?php
+/** op-asset-git:/pull.php
+ *
+ * <pre>
+ * ```sh
+ * php app.php _develop/git/pull
+ * ```
+ * </pre>
+ *
+ * @created    2025-10-01
+ * @version    1.0
+ * @package    op-asset-git
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
