4dfdaf3b0a264b8ddedecca0e7b5ad8e0a01c01a
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-25 18:32:07 +0900

    Chg: submodule > foreach.php | Refactoring.

--- submodule/foreach.php
+++ submodule/foreach.php
@@ -19,29 +19,31 @@ declare(strict_types=1);
 namespace OP;
 
 //	...
-if(!$foreach = OP()->Request('foreach') ){
-	?>
+if( $foreach = OP()->Request('foreach') ){
+
+	//	...
+	$configs = OP()->Unit()->Git()->SubmoduleConfig();
+
+	//	...
+	echo PHP_EOL;
+	foreach( $configs as $name => $config ){
+		chdir(_ROOT_GIT_.$config['path']);
+		echo "$name: " . getcwd() . PHP_EOL;
+		echo PHP_EOL;
+		echo `{$foreach}`.PHP_EOL;
+		/*
+		echo `git log --pretty=format:"%h %an <%ae> %ad %cn <%ce> %cd" --date=format:'%Y-%m-%d %H:%M:%S' | grep @g`;
+		*/
+	}
+
+	//	...
+	return;
+}
+
+?>
 
 # Usage
 
 ```
 php app.php _develop/git command=submodule/foreach "foreach=git remote -v"
 ```
-	<?php
-	return;
-}
-
-//	...
-$configs = OP()->Unit()->Git()->SubmoduleConfig();
-
-//	...
-echo PHP_EOL;
-foreach( $configs as $name => $config ){
-	chdir(_ROOT_GIT_.$config['path']);
-	echo "$name: " . getcwd() . PHP_EOL;
-	echo PHP_EOL;
-	echo `{$foreach}`.PHP_EOL;
-	/*
-	echo `git log --pretty=format:"%h %an <%ae> %ad %cn <%ce> %cd" --date=format:'%Y-%m-%d %H:%M:%S' | grep @g`;
-	*/
-}
