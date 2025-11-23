495da4efb95352a59a4e4014ef5f6d4fe35ff99d
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-24 03:36:11 +0900

    Add: content > index.php | PHPDoc

---
+++ content/index.php
@@ -0,0 +1,19 @@
+<?php
+/**	op-module-git:/content/index.php
+ *
+ * @created    2025-11-24
+ * @version    1.0
+ * @package    op-module-git
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All rights reserved.
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
